c66a084ae865ffa1538cbc3306eb19452e7be70b
Belzico <namichurri@gmail.com>
2022-07-29 07:32:29 +0200

    mini

--- src/Play.Common/IEntity.cs
+++ src/Play.Common/IEntity.cs
@@ -5,10 +5,6 @@ namespace Play.Common
     public interface IEntity
     {
         Guid Id { get; set; }
-        string Name { get; set; }
-        string Description { get; set; }
-        decimal Price { get; set; }
-        DateTimeOffset CreatedDate { get; set; }
     }
 }
 
